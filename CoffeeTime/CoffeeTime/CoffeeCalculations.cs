6f772ef838351cf02b2c40436c1534a2b3643539
Baldur Tryggvason <ballit85@gmail.com>
2017-01-05 14:46:18 +0000

    Initial commit

---
+++ CoffeeTime/CoffeeTime/CoffeeCalculations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeTime
+{
+    class CoffeeCalculations
+    {
+        private const int CoffeeCupSizeMl = 150;
+        private const double FrenchPressGramsPerCup = 12.5;
+
+        public int CupsToMillileters(int cups)
+        {
+            return cups * CoffeeCupSizeMl;
+        }
+
+        public double CupsToCoffeeGrams(string brewMethod, int cups)
+        {
+            if (brewMethod == "French Press")
+            {
+                return FrenchPressGramsPerCup * cups;
+            }
+            else
+            {
+                return 15;
+            }
+        }
+    }
+}
---
+++ CoffeeTime/CoffeeTime/Pages/CoffeeSelectionPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CoffeeTime.Pages
+{
+    public partial class CoffeeSelectionPage : ContentPage
+    {
+        private readonly CoffeeCalculations _coffeeCalculations;
+
+        public CoffeeSelectionPage()
+        {
+            _coffeeCalculations = new CoffeeCalculations();
+
+            InitializeComponent();
+
+            CoffeeCupSelectionText.Text = $"Cups: {CoffeeCupStepper.Value}";
+            CoffeeGramSlider.Value = _coffeeCalculations.CupsToCoffeeGrams(CoffeeMethodPicker.Items[CoffeeMethodPicker.SelectedIndex],
+                    Convert.ToInt32(CoffeeCupStepper.Value));
+            CoffeeGramsText.Text = $"{CoffeeGramSlider.Value:F1}";
+        }
+
+        private void CoffeeCupStepper_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            CoffeeCupSelectionText.Text = $"Cups: {e.NewValue}";
+            CoffeeGramSlider.Value = _coffeeCalculations.CupsToCoffeeGrams(CoffeeMethodPicker.Items[CoffeeMethodPicker.SelectedIndex],
+                    Convert.ToInt32(CoffeeCupStepper.Value));
+        }
+
+        private void CoffeeGramSlider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //CoffeeGramsText.Text = Convert.ToInt32(e.NewValue).ToString();
+            CoffeeGramsText.Text = $"{e.NewValue:F1}";
+        }
+    }
+}
